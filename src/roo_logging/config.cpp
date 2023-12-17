1a159e9869c96e0735c2dc73171696d8a67ea7d9
dejwk <dawid.kurzyniec@gmail.com>
2023-12-17 16:03:29 +0100

    Colored stderr: default to false, since it requires some special setup of the terminal to work.

--- src/roo_logging/config.cpp
+++ src/roo_logging/config.cpp
@@ -3,5 +3,5 @@
 ROO_FLAG(roo_logging::WallTimeClockPtr, roo_logging_wall_time_clock, nullptr);
 ROO_FLAG(roo_time::TimeZone, roo_logging_timezone, roo_time::timezone::UTC);
 ROO_FLAG(bool, roo_logging_prefix, true);
-ROO_FLAG(bool, roo_logging_colorlogtostderr, true);
+ROO_FLAG(bool, roo_logging_colorlogtostderr, false);
 ROO_FLAG(uint8_t, roo_logging_minloglevel, 0);
